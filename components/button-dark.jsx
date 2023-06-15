c33b13cf19640717fbe3a04fd61a9fc20d257c15
joaomonteiro <70484799+joaomonteiroSN@users.noreply.github.com>
2023-06-15 16:39:37 -0300

    criançao da section 1 - incompleto

---
+++ components/button-dark.jsx
@@ -0,0 +1,7 @@
+import styles from '../src/app/page.module.css'
+
+export function DarkButton() {
+    return (
+      <button className={styles.darkButton}>Join Now</button>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,32 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { DarkButton } from '../../components/button-dark'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <header className={styles.header}>
+        <div className={styles.center}>
+          <img src='Logo.svg' className={styles.logoStyle}></img>
+          <div className={styles.navButtons}>
+            <div className={styles.links}>
+              <a className={styles.click} href='_blank'>For Projects</a>
+              <a className={styles.click} href='_blank'>For Investors</a>
+            </div>
+            <div>
+              <DarkButton></DarkButton>
+            </div>
+          </div>
         </div>
-      </div>
+      </header>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.sectionOne}>
+        <h1 className={styles.title}>Vision Capital</h1>
+        <p className={styles.pTitle}>The right way to fund and incubate your DeFi startup</p>
+        <div className={styles.buttonsTitle}>
+          <DarkButton></DarkButton>
+          <DarkButton></DarkButton>
+        </div>
       </div>
     </main>
   )
